f1e4d8418a96c5113fbba835e60b35904c457e18
subbni <slfkak@naver.com>
2024-04-23 11:05:55 +0900

    [BOJ] 열차정렬(4198) / S / LIS, LDS

--- boj/lis/Boj4198_열차정렬.java
+++ boj/lis/Boj4198_열차정렬.java
@@ -11,37 +11,27 @@ public class Boj4198_열차정렬 {
         int[] arr = new int[N];
         int[] inDp = new int[N];
         int[] deDp = new int[N];
+        int ans = 0;
         for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i=0; i<N; i++) {
-            inDp[i] = 1;
-            deDp[i] = 1;
-            for(int j=0; j<i; j++) {
-                if(arr[i] > arr[j]) {
+        for(int i=N-1; i>=0; i--) {
+            for(int j=i+1; j<N; j++) {
+                if(arr[j] > arr[i]) {
                     // LIS
-                    inDp[i] = Math.max(inDp[i], inDp[j]+1);
+                    inDp[i] = Math.max(inDp[i],inDp[j]);
                 } else {
                     // LDS
-                    deDp[i] = Math.max(deDp[i], deDp[j]+1);
+                    deDp[i] = Math.max(deDp[i],deDp[j]);
                 }
             }
+
+            inDp[i]++;
+            deDp[i]++;
+            ans = Math.max(ans, inDp[i]+deDp[i]-1);
         }
 
-        int inMax = 0;
-        int idx = 0;
-        int deMax = 0;
-        for(int i=0; i<N; i++) {
-            if(inMax < inDp[i]) {
-                inMax = inDp[i];
-                idx = i;
-            }
-        }
-        for(int i=0; i<idx; i++) {
-            if(deMax < deDp[i]) deMax = deDp[i];
-        }
-
-        System.out.println(N==0? 0: inMax+deMax-1);
+        System.out.println(ans);
     }
 }
